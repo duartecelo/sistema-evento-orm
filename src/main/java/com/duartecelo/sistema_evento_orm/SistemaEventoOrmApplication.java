cfcebde3328c69925c6a12d0f08459de0ce37db1
Marcelo Duarte <celo.duarte.2005@gmail.com>
2025-02-19 03:15:35 -0300

    primeiro commit

---
+++ src/main/java/com/duartecelo/sistema_evento_orm/SistemaEventoOrmApplication.java
@@ -0,0 +1,13 @@
+package com.duartecelo.sistema_evento_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventoOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventoOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duartecelo/sistema_evento_orm/SistemaEventoOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duartecelo.sistema_evento_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
